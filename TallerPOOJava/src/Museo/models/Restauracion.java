12fafad17afb3ecff711b180fe6efa8fbdefa080
Dorozco89 <dorozco89@unisalle.edu.co>
2026-04-02 20:56:44 -0500

    Clase Restauracion

---
+++ TallerPOOJava/src/Museo/models/Restauracion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */ 
+/**
+ *
+ * @author USUARIO
+ */
+package Museo.models;
+
+public class Restauracion {
+
+    private final String tipo;
+    private final String fechaInicio;
+    private String fechaFin;
+
+    public Restauracion(String tipo, String fechaInicio) {
+        this.tipo = tipo;
+        this.fechaInicio = fechaInicio;
+    }
+
+    public void finalizar(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+}
