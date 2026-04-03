fe524ff4cf8f540037485e96b18dfdca87c4f3bd
Dorozco89 <dorozco89@unisalle.edu.co>
2026-04-02 22:18:34 -0500

    Clase EncargadoCatalogo

---
+++ TallerPOOJava/src/Museo/models/EncargadoCatalogo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author USUARIO
+ */
+package museo.models;
+
+public class EncargadoCatalogo extends Usuario {
+
+    public EncargadoCatalogo(int id, String nombre,
+                              String correo, String password) {
+        super(id, nombre, correo, password);
+    }
+
+    public void registrarObra(Catalogo catalogo, ObraArte obra) {
+        catalogo.agregarObra(obra);
+    }
+}
