07b22f9cb2f3df4ec6ec207cd0df1fc9738f06e5
Dorozco89 <dorozco89@unisalle.edu.co>
2026-04-02 21:50:24 -0500

    Clase Sala

---
+++ TallerPOOJava/src/Museo/models/Sala.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author USUARIO
+ */
+package museo.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sala {
+
+    private String nombre;
+    private List<ObraArte> obras;
+
+    public Sala(String nombre) {
+        this.nombre = nombre;
+        this.obras = new ArrayList<>();
+    }
+
+    public void agregarObra(ObraArte obra) {
+        obras.add(obra);
+    }
+
+    public List<ObraArte> listarObras() {
+        return obras;
+    }
+}
