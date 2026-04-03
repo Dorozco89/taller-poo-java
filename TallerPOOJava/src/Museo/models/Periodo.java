bf19a2c9fbc0523c10b57e04f45ca9770f42a63e
Dorozco89 <dorozco89@unisalle.edu.co>
2026-04-02 20:29:46 -0500

    Clase Periodo

---
+++ TallerPOOJava/src/Museo/models/Periodo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author USUARIO
+ */
+package Museo.models;
+
+public class Periodo {
+
+    private final String nombre;
+    private final String descripcion;
+
+    public Periodo(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
